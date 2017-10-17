715e1aa16f3ce45604caf4887f414063f95c3e1c
Dimitrij <kvarkas@gmail.com>
2017-10-17 19:36:39 +0100

    replace windows CRLF by Unix LF

--- install-apache.sh
+++ install-apache.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+. /usr/local/osmosix/service/utils/agent_util.sh
+agentSendLogMessage "SCRIPT MESSAGE: update to the last"
 sudo yum update -y
-sudo yum install httpd
-sudo mkdir -p /var/www/html/example.com/{public_html,logs}
+agentSendLogMessage "SCRIPT MESSAGE: Install apache"
+sudo yum install httpd -y
 sudo systemctl enable httpd.service
 sudo systemctl restart httpd.service
+agentSendLogMessage "SCRIPT MESSAGE: Create demo vhost"
+sudo mkdir -p /var/www/html/example.com/{public_html,logs}
+agentSendLogMessage "SCRIPT MESSAGE: Enable http and https protocols"
 sudo firewall-cmd --zone=public --permanent --add-service=http
 sudo firewall-cmd --zone=public --permanent --add-service=https
 sudo firewall-cmd --reload
+agentSendLogMessage "SCRIPT MESSAGE: Node setup completed"
